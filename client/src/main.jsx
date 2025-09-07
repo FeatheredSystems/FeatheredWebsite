abd05caf3583ea5f7c23d848c4c285e58b9cf6c8
theo <178332918+ttd3v@users.noreply.github.com>
2025-09-06 21:58:53 -0300

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router, Route } from "react-router-dom"
 import './index.css'
+import TytoDBPage from './tytodb/tytodb'
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <Route path='/'></Route>
-    <Route path='/TytoDB/:content'></Route>
+    <Route element={<TytoDBPage/>} path='/TytoDB/:content'></Route>
   </Router>
 )
--- client/src/tytodb/tytodb.jsx
+++ client/src/tytodb/tytodb.jsx
@@ -1,9 +1,46 @@
 import { useParams } from "react-router-dom"
+import ReactMarkdown from 'react-markdown';
+import { useEffect, useState } from "react";
 
+async function fetch_meta() {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/FeatheredSystems/FeatheredWebsite/refs/heads/main/data/docs.json");
+    const json = await response.json();
+    return json["TytoDB"] || {}; 
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+}
+
+async function get_text(id){
+    try {
+    const response = await fetch(`https://raw.githubusercontent.com/FeatheredSystems/FeatheredWebsite/refs/heads/main/data/docs_text/TytoDB/${id}.md`);
+    const txt = await response.text();
+    return txt || ""; 
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+}
 
 function TytoDBPage(){
     const {content} = useParams();
-    
+    const [meta,setMeta] = useState({})
+    const [text,setText] = useState("");
+
+    useEffect(async () => {
+        setMeta(await fetch_meta());
+    },[])
+    useEffect(async () => {
+        if (!Object.is(meta,{})){
+            setText(await get_text(meta[content]))
+        }
+    },[meta])
+
+    return (<>
+        <ReactMarkdown>{text}</ReactMarkdown>
+    </>)
 }
 
 export default TytoDBPage
